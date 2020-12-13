de452148d1840435d59f7ce3d3bfd0eaf96b6c7e
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-12-13 15:50:56 +0700

    Update Uts.py

--- Uts.py
+++ Uts.py
@@ -5,25 +5,25 @@ print("2. Ambil Uang Saya.")
 print("3. Tabung Uang Saya.")
 option=int(input("Silahkan Pilih Option : "))
 if option ==1:
-  print("Uang Kamu Berjumlah : Rp. 200.000")
+    print("Uang Kamu Berjumlah : Rp. 200.000")
 elif option ==2:
-  print("Uang Kamu Berjumlah Rp. 200.000, Mau Ambil Berapa?")
-  print("1. Rp.100.000")
-  print("2. Rp.200.000")
-  uang_kamu = 200000
-  option2=int(input("Option :")
-  if option2==1:
-    hasil=uang_kamu-100000
-    print("Uang Kamu Sekarang Berjumlah : Rp.", hasil)
+    print("Uang Kamu Berjumlah Rp. 200.000, Mau Ambil Berapa?")
+    print("1. Rp.100.000")
+    print("2. Rp.200.000")
+    uang_kamu = 200000
+    option2=int(input("Option :"))
+    if option2==1:
+        hasil=uang_kamu-100000
+        print("Uang Kamu Sekarang Berjumlah : Rp.", hasil)
     elif option2==2:
-      hasil2=uang_kamu-200000
-      print("Uang Kamu Sekarang Berjumlah : Rp.", hasil2)
+        hasil2=uang_kamu-200000
+        print("Uang Kamu Sekarang Berjumlah : Rp.", hasil2)
     else :
-      print("Keyword Yang Anda Masukkan Salah, Coba Ulangi lagi!!")
- elif option3==3:
+         print("Keyword Yang Anda Masukkan Salah, Coba Ulangi lagi!!")
+elif option3==3:
     hasil3=uang_kamu=200000
     print("Uang berjumlah Rp.200.000, Mau nabung Berapa?")
-    optiont3=int(input("Masukan Jumlah Uang :"))
+    option3=int(input("Masukan Jumlah Uang :"))
     hasil3=uang_kamu+option3
     print("Jumlah Uang Kamu Sekarang Adalah ",hasil3)
 else :
