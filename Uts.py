008846acc034b61138e3c2e319ebe8190fa1962e
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-12-13 12:47:40 +0700

    Update Uts.py

--- Uts.py
+++ Uts.py
@@ -1,1 +1,24 @@
 print("Selamat Datang Di ATM NIPON")
+print("Pilih Option : ")
+print("1. Check Uang Saya.")
+print("2. Ambil Uang Saya.")
+print("3. Tabung Uang Saya.")
+option=int(input("Silahkan Pilih Option : "))
+if option ==1:
+  print("Uang Kamu Berjumlah : Rp. 200.000")
+elif option ==2:
+  print("Uang Kamu Berjumlah Rp. 200.000, Mau Ambil Berapa?")
+  print("1. Rp.100.000")
+  print("2. Rp.200.000")
+  uang_kamu = 200000
+  if option2==1:
+    hasil=uang_kamu-100000
+    print("Uang Kamu Sekarang Berjumlah : Rp.", hasil)
+    elif option2==2:
+      hasil2=uang_kamu-200000
+      print("Uang Kamu Sekarang Berjumlah : Rp.", hasil2)
+    else :
+      print("Keyword Yang Anda Masukkan Salah, Coba Ulangi lagi!!")
+      
+      
+      
